bb89834b857832d0f8c86d51ead58797355db86a
Melkiiche1 <xixixixxddjddj@gmail.com>
2026-04-06 20:29:53 +0300

    creat Task3 ma

--- Queue/src/Task2.java
+++ Queue/src/Task2.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class Task2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter u number");
+        int number = sc.nextInt();
+
+        int[] arr = new int[number];
+
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0 || ((i == 0 || arr[i] != arr[i - 1]) && (i == arr.length - 1 || arr[i] != arr[i + 1]))) {
+                list.add(arr[i]);
+            }
+        }
+
+        System.out.println(list);
+
+    }
 }
+
