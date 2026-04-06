e89b4bc06b350f3ccbcf7bb0dacc21d36af5c228
Melkiiche1 <xixixixxddjddj@gmail.com>
2026-04-06 21:39:50 +0300

    taska 3 ready

--- Queue/src/Task3.java
+++ Queue/src/Task3.java
@@ -1,11 +1,35 @@
 import java.util.Scanner;
 import java.util.Deque;
+import java.util.Queue;
 import java.util.LinkedList;
+import java.util.Stack;
+
 
 public class Task3 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        LinkedList<Integer> queue = new LinkedList<>();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Queue<Integer> queue = new LinkedList<>();
+        Stack<Integer> stack = new Stack<>();
+
+        String input = scanner.nextLine();
+        String[] parts = input.split("\\s+");
+
+        for (String part : parts) {
+                int num = Integer.parseInt(part);
+                if (num <= 10 && num % 2 != 0) {
+                    queue.add(num);
+                }
+        }
+
+        while (!queue.isEmpty()) {
+            stack.push(queue.poll());
+        }
+
+        while (!stack.isEmpty()) {
+            queue.add(stack.pop());
+        }
+
+        System.out.println(queue);
     }
 }
